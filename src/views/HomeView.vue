052933ab8b8813391a72a479355bac076fc478b4
Koban <23511389+koba0124@users.noreply.github.com>
2025-01-19 20:07:02 +0900

    setupに書き換え

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,7 @@
+<script setup lang="ts">
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
 <template>
   <hello-world />
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
-</script>
