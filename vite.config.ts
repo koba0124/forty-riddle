f40e5ebd7eb2ab85293d168fd7e86dbd9c11f9d2
Koban <23511389+koba0124@users.noreply.github.com>
2025-01-21 03:59:49 +0900

    outDir

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
